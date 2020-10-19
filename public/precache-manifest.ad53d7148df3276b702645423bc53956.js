f3c184fa39a22e828ef12aee54b4a856001d1f45
rschpdr <pedro.resch@gmail.com>
2020-10-18 22:29:38 -0300

    added built react app

---
+++ public/precache-manifest.ad53d7148df3276b702645423bc53956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a429e0860a0bdc5f0d8710c944691a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79d0ec2874e43b6fddd4",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "79d0ec2874e43b6fddd4",
+    "url": "/static/js/2.323a6797.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.323a6797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a00626e30cb7a30461e",
+    "url": "/static/js/main.5fe78030.chunk.js"
+  },
+  {
+    "revision": "98dbf1210ca2acfabfd9",
+    "url": "/static/js/runtime-main.88aca0f0.js"
+  }
+]);
